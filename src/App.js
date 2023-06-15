6d9668cc15777054f928b1fd3531a09ea2f62acd
Masaabushamma99 <Masaabushamma402@gmail.com>
2023-06-15 06:39:18 +0300

    final edit for router urls and deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react';
 import DesignBanner from './component/DesignBanner';
 import Footer from './component/Footer';
 import Home from './pages/Home'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Details from './pages/Details';
 
 function App() {
@@ -23,8 +23,8 @@ function App() {
         <Header toggleFavoriteSlide={toggleFavoriteSlide} />
         <DesignBanner />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="Details/:id" element={<Details />} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/Details/:id" element={<Details />} />
         </Routes><Footer />
         {isFavouritePanelOpen &&
           <FavouriteBanner />}
