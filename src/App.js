67be5ffa3cce3f42e4c809c319d57dbac4fe5e3b
Masaabushamma99 <Masaabushamma402@gmail.com>
2023-06-15 08:31:17 +0300

    fix deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react';
 import DesignBanner from './component/DesignBanner';
 import Footer from './component/Footer';
 import Home from './pages/Home'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Details from './pages/Details';
 
 function App() {
